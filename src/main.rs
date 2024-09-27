2481d2aa5f8451cb5a1c80c5757dc30628e2c5ea
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-09-27 20:20:05 +0530

    fuck this

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{Arg, Command};
-use reqwest::Client;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = Command::new("My Program")
         .version("1.0")
         .author("shashank the great")
@@ -46,34 +46,68 @@ fn main() {
     let mut value = String::new();
     if let Some(matches) = matches.subcommand_matches("search") {
         value = matches.get_one::<String>("SEARCH").unwrap().to_owned();
-    }
 
-    else if let Some(matches) = matches.subcommand_matches("info") {
+        let _res = fetch_manga_with_similar_names(&value).await?;
+    } else if let Some(matches) = matches.subcommand_matches("info") {
         value = matches.get_one::<String>("INFO").unwrap().to_owned();
-    }
-
-    else if let Some(matches) = matches.subcommand_matches("download") {
+    } else if let Some(matches) = matches.subcommand_matches("download") {
         value = matches.get_one::<String>("DOWNLOAD").unwrap().to_owned();
     }
 
     println!("The value is: {}", value);
+    return Ok(());
 }
 
 #[allow(dead_code)]
-async fn fetch_manga_list(query: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new();
-    let response = client.get(format!("https://api.comick.fun/comic/{}/chapters?lang=en&limit=99999&tachiyomi=true",query))
-    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    .header("Accept", "application/json, text/plain, */*")
-    .header("Accept-Language", "en-US,en;q=0.9")
-    .header("Referer", "https://comick.fun/")
-    .header("Origin", "https://comick.fun")
-    .header("Sec-Fetch-Dest", "empty")
-    .header("Sec-Fetch-Mode", "cors")
-    .header("Sec-Fetch-Site", "same-site")
-    .send().await?;
+async fn fetch_manga_with_similar_names(query: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let url = format!(
+        "https://api.comick.fun/v1.0/search?q={}&tachiyomi=true",
+        query
+    );
+    let header = headers();
+    let res = client.get(&url).headers(header).send().await?;
 
-    let body = response.text().await?;
-    println!("{}", body);
-    Ok(())
+    match res.status() {
+        reqwest::StatusCode::OK => println!("i got something"),
+        reqwest::StatusCode::FORBIDDEN => println!("it is saying forbidden"),
+        _ => println!("i don't care"),
+    }
+    return Ok(());
+}
+
+#[allow(dead_code)]
+fn headers() -> reqwest::header::HeaderMap {
+    let mut header = reqwest::header::HeaderMap::new();
+    header.insert("User-Agent", reqwest::header::HeaderValue::from_static("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"));
+    header.insert(
+        "Accept",
+        reqwest::header::HeaderValue::from_static("application/json, text/plain, */*"),
+    );
+    header.insert(
+        "Accept-Language",
+        reqwest::header::HeaderValue::from_static("en-US,en;q=0.9"),
+    );
+    header.insert(
+        "Referer",
+        reqwest::header::HeaderValue::from_static("https://comick.fun/"),
+    );
+    header.insert(
+        "Origin",
+        reqwest::header::HeaderValue::from_static("https://comick.fun"),
+    );
+    header.insert(
+        "Sec-Fetch-Dest",
+        reqwest::header::HeaderValue::from_static("empty"),
+    );
+    header.insert(
+        "Sec-Fetch-Mode",
+        reqwest::header::HeaderValue::from_static("cors"),
+    );
+    header.insert(
+        "Sec-Fetch-Site",
+        reqwest::header::HeaderValue::from_static("same-site"),
+    );
+
+    return header;
 }
